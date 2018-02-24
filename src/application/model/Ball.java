3f6f44d65124e4138447a1c769424564971dddf4
Daniel Russom <daniel.russom00@gmail.com>
2018-02-24 12:50:08 +0000

    Model init
    -Initial model classes

---
+++ src/application/model/Ball.java
@@ -0,0 +1,12 @@
+package application.model;
+
+import javafx.scene.Node;
+
+public class Ball {
+	Node image;
+	
+	public Ball() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/application/model/Paddle.java
@@ -0,0 +1,12 @@
+package application.model;
+
+import javafx.scene.Node;
+
+public class Paddle {
+	Node image;
+	
+	public Paddle() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
